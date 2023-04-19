9c9bd66d44cacba527969de47227e2604a0e3521
Miran <miran_sert@live.dk>
2023-04-19 10:26:01 +0200

    changed resolution

--- Core/src/main/java/main/Main.java
+++ Core/src/main/java/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("Zombie game");
-        int width = 1472;
+        int width = 1440;
         int height = 736;
         cfg.setWindowSizeLimits(width, height, width, height);
         cfg.setWindowedMode(width, height);
