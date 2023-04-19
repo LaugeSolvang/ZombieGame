0799ee659c26cdae2b6c78176071c5fbd02155b2
LaugeSolvang <lasol21@student.sdu.dk>
2023-04-19 10:20:58 +0200

    Changed playerprocessing more maintainable code

--- Player/src/main/java/playersystem/PlayerProcessor.java
+++ Player/src/main/java/playersystem/PlayerProcessor.java
@@ -16,41 +16,51 @@ import static common.data.GameKeys.*;
 
 
 public class PlayerProcessor implements IEntityProcessingService {
-
     @Override
     public void process(GameData gameData, World world) {
         for (Entity playerEntity : world.getEntities(Player.class)) {
             Player player = (Player) playerEntity;
-            PositionPart positionPart = player.getPart(PositionPart.class);
-            MovingPart movingPart = player.getPart(MovingPart.class);
 
-            movingPart.setLeft(gameData.getKeys().isDown(LEFT));
-            movingPart.setRight(gameData.getKeys().isDown(RIGHT));
-            movingPart.setUp(gameData.getKeys().isDown(UP));
-            movingPart.setDown(gameData.getKeys().isDown(DOWN));
+            processMovement(gameData, player);
+            processWeapon(gameData, world, player);
+            processWeaponSwitching(gameData, player);
+        }
+    }
 
-            movingPart.process(gameData, player);
-            positionPart.process(gameData, player);
+    private void processMovement(GameData gameData, Player player) {
+        MovingPart movingPart = player.getPart(MovingPart.class);
+        movingPart.setLeft(gameData.getKeys().isDown(LEFT));
+        movingPart.setRight(gameData.getKeys().isDown(RIGHT));
+        movingPart.setUp(gameData.getKeys().isDown(UP));
+        movingPart.setDown(gameData.getKeys().isDown(DOWN));
+        movingPart.process(gameData, player);
 
-            //Checks if the player has any weapons
-            if (player.getWeapons().size() != 0) {
-                //Sets the PositionPart of the weapon to the players so the weapon follows the player
-                Weapon weapon = player.getCurrentWeapon();
-                PositionPart positionPartWeapon = weapon.getPart(PositionPart.class);
-                positionPartWeapon.setPosition(positionPart.getX(), positionPart.getY());
+        PositionPart positionPart = player.getPart(PositionPart.class);
+        positionPart.process(gameData, player);
+    }
 
-                //Gets the appropriate IShoot implementation if conditions are true
-                if (gameData.getKeys().isPressed(SPACE) && weapon.getAmmo() > 0) {
-                    IShoot shootImpl = getShootImpl(weapon);
-                    shootImpl.useWeapon(player, gameData, world);
-                } else if (weapon.getAmmo() == 0) {
-                    world.removeEntity(weapon);
-                }
+    private void processWeapon(GameData gameData, World world, Player player) {
+        if (!player.getWeapons().isEmpty()) {
+            Weapon weapon = player.getCurrentWeapon();
+            PositionPart playerPositionPart = player.getPart(PositionPart.class);
+            PositionPart weaponPositionPart = weapon.getPart(PositionPart.class);
+
+            weaponPositionPart.setPosition(playerPositionPart.getX(), playerPositionPart.getY());
+
+            if (gameData.getKeys().isPressed(SPACE) && weapon.getAmmo() > 0) {
+                IShoot shootImpl = getShootImpl(weapon);
+                shootImpl.useWeapon(player, gameData, world);
+            } else if (weapon.getAmmo() == 0) {
+                world.removeEntity(weapon);
             }
-            //Checks if the one or two key is pressed and cycles the player's weapons accordingly
-            if (gameData.getKeys().isPressed(ONE) || gameData.getKeys().isPressed(TWO)) {
-                player.cycleWeapon(gameData.getKeys().isPressed(ONE) ? -1 : 1);
-            }
+        }
+    }
+
+    private void processWeaponSwitching(GameData gameData, Player player) {
+        if (gameData.getKeys().isPressed(ONE)) {
+            player.cycleWeapon(-1);
+        } else if (gameData.getKeys().isPressed(TWO)) {
+            player.cycleWeapon(1);
         }
     }
     private IShoot getShootImpl(Weapon weapon) {
