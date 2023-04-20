f86f767254d21613dc58f7b77d117f04f6cbb4dc
LaugeSolvang <lasol21@student.sdu.dk>
2023-04-20 22:44:04 +0200

    Continuing still doesn't work.

--- ZombieAI/src/main/java/zombieaisystem/AStar.java
+++ ZombieAI/src/main/java/zombieaisystem/AStar.java
@@ -3,7 +3,24 @@ package zombieaisystem;
 import java.util.*;
 
 public class AStar {
+    public static void main(String[] args) {
+        int[][] map = new int[30][15];
 
+        //Spawn obstructions around the perimeter of the game
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (i == 0 || j == 0 || i == map.length-1 || j == map[0].length-1) {
+                    map[i][j] = 1;
+                } else {
+                    map[i][j] = 0;
+                }
+            }
+        }
+        AStar aStar = new AStar();
+        System.out.println(aStar.treeSearch(map, new int[]{2,2},new int[]{10,10}));
+        //STATE_SPACE = generateStateSpace(map);
+        //printStateSpace(STATE_SPACE);
+    }
     class Node {
         public int[] STATE;
         public Node PARENT_NODE;
@@ -27,25 +44,18 @@ public class AStar {
             }
             return path;
         }
-
-        public void display() {
-            System.out.println(this.toString());
-        }
-
-        @Override
-        public String toString() {
-            return "State " + java.util.Arrays.toString(STATE) + " Depth " + DEPTH + " Cost " + COST;
-        }
     }
 
-    public class TreeSearch {
-        private static Map<int[], List<int[]>> STATE_SPACE = null;
-        private int[][] HEURISTICS = null;
-        private int[][] COST = null;
+
+        private static Map<int[], List<int[]>> STATE_SPACE;
+        private Map<int[], Integer> HEURISTICS;
 
         public ArrayList<Node> treeSearch(int[][] grid, int[] INITIAL_STATE, int[] GOAL_STATE) {
             if (STATE_SPACE == null) {
                 STATE_SPACE = generateStateSpace(grid);
+            }
+            if (HEURISTICS == null) {
+                HEURISTICS = generateHeuristics(grid, GOAL_STATE);
             }
 
             ArrayList<Node> fringe = new ArrayList<>();
@@ -59,9 +69,9 @@ public class AStar {
                 ArrayList<Node> children = EXPAND(node);
                 fringe = INSERT_ALL(children, fringe);
                 for (Node child : children) {
-                    child.COST = node.COST + COST[node.COST][child.COST];
+                    child.COST = node.COST + 1;
                 }
-                fringe.sort(Comparator.comparingInt(x -> HEURISTICS[x.STATE[0]][x.STATE[1]] + x.COST));
+                fringe.sort(Comparator.comparingInt(x -> HEURISTICS.get(x.STATE) + x.COST));
                 System.out.println("fringe: " + fringe);
             }
             return null;
@@ -92,6 +102,8 @@ public class AStar {
         }
 
         public static List<int[]> successor_fn(int[] state) {
+            System.out.println(java.util.Arrays.toString(state));
+            System.out.println(STATE_SPACE.get(state));
             return STATE_SPACE.get(state);
         }
 
@@ -99,12 +111,15 @@ public class AStar {
             Map<int[], List<int[]>> stateSpace = new HashMap<>();
             for (int i = 0; i < grid.length; i++) {
                 for (int j = 0; j < grid[0].length; j++) {
-                    int [] state = new int[]{i, j};
-                    stateSpace.put(state, generateNeighbours(grid, state));
+                    if (grid[i][j] != 1) {
+                        int[] state = new int[]{i, j};
+                        stateSpace.put(state, generateNeighbours(grid, state));
+                    }
                 }
             }
             return stateSpace;
         }
+
         // Method to get a list of a node's neighbors on the grid
         private static List<int[]> generateNeighbours(int[][] grid, int[] STATE) {
             List<int[]> neighbors = new ArrayList<>();
@@ -118,9 +133,34 @@ public class AStar {
                 if (grid[x][y] == 1) {
                     continue;
                 }
-                neighbors.add(STATE);
+                neighbors.add(new int[]{x,y});
             }
             return neighbors;
+        }
+
+        public Map<int[], Integer> generateHeuristics(int[][] grid, int[] GOAL_STATE) {
+            Map<int[], Integer> heuristics = new HashMap<>();
+            for (int x = 0; x < grid.length; x++) {
+                for (int y = 0; y < grid[x].length; y++) {
+                    int dx = Math.abs(x - GOAL_STATE[0]);
+                    int dy = Math.abs(y - GOAL_STATE[1]);
+                    heuristics.put(new int[]{x, y}, dx + dy);
+                }
+            }
+            return heuristics;
+        }
+
+    public static void printStateSpace(Map<int[], List<int[]>> stateSpace) {
+        TreeSet<int[]> sortedStates = new TreeSet<>(Comparator.comparingInt((int[] o) -> o[0]).thenComparingInt(o -> o[1]));
+        sortedStates.addAll(stateSpace.keySet());
+
+        for (int[] state : sortedStates) {
+            System.out.print("State: " + Arrays.toString(state) + " Neighbours: ");
+            List<int[]> neighbours = stateSpace.get(state);
+            for (int[] neighbour : neighbours) {
+                System.out.print(Arrays.toString(neighbour) + " ");
+            }
+            System.out.println();
         }
     }
 }
