43beef547db5eb6aa1b25f864b8c934aa2c0e1f9
LaugeSolvang <lasol21@student.sdu.dk>
2023-04-20 16:20:39 +0200

    changed the acceleration for player and zombie and changed how the zombie finds the player this is not AI

--- Map/src/main/java/mapsystem/MapPlugin.java
+++ Map/src/main/java/mapsystem/MapPlugin.java
@@ -4,43 +4,29 @@ import common.data.Entity;
 import common.data.GameData;
 import common.data.World;
 import common.data.entities.obstruction.Obstruction;
-import common.data.entities.weapon.WeaponSPI;
-import common.data.entities.zombie.ZombieSPI;
 import common.data.entityparts.PositionPart;
 import common.services.IGamePluginService;
 
-import java.util.Collection;
-import java.util.ServiceLoader;
-
-import static java.util.stream.Collectors.toList;
-
 public class MapPlugin implements IGamePluginService {
-    String[][] map;
+    int[][] map;
     @Override
     public void start(GameData gameData, World world)  {
         //Create a map with strings per 32x32 pixels of the whole display
-        map = new String[gameData.getDisplayHeight()/32][gameData.getDisplayWidth()/32];
+        map = new int[gameData.getDisplayWidth()/32][gameData.getDisplayHeight()/32];
 
         //Spawn obstructions around the perimeter of the game
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
-                if (i == 0 || j == 0 || i == map.length-1 || j == map[0].length-1) {
-                    Entity obstruction = createObstruction(j * 32, i * 32);
+                if (i == 0 || j == 0 || i == map.length-1 || j == map[0].length-1 || i%6==0 && j%6==0) {
+                    Entity obstruction = createObstruction(i * 32, j * 32);
                     world.addEntity(obstruction);
-                    map[i][j] = "obstruction";
+                    map[i][j] = 1;
+                } else {
+                    map[i][j] = 0;
                 }
             }
         }
-        //Spawn an obstruction closer to the centre
-        world.addEntity(createObstruction(gameData.getDisplayWidth()/2-64, gameData.getDisplayHeight()/2-64));
-
-        for (WeaponSPI weapon : getWeaponSPI()) {
-            world.addEntity(weapon.createWeapon(gameData.getDisplayWidth()/2+64, gameData.getDisplayHeight()/2+64));
-        }
-
-        for (ZombieSPI zombie : getZombieSPI()) {
-            world.addEntity(zombie.createZombie(gameData.getDisplayWidth()/2+128, gameData.getDisplayHeight()/2+128));
-        }
+        world.setMap(map);
     }
 
     @Override
@@ -57,21 +43,4 @@ public class MapPlugin implements IGamePluginService {
 
         return obstruction;
     }
-
-    public String[][] getMap() {
-        return map;
-    }
-
-    public void setMap(String[][] map) {
-        this.map = map;
-    }
-
-    protected Collection<? extends WeaponSPI> getWeaponSPI() {
-        return ServiceLoader.load(WeaponSPI.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    protected Collection<? extends ZombieSPI> getZombieSPI() {
-        return ServiceLoader.load(ZombieSPI.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
 }
--- Map/src/main/java/mapsystem/MapProcessor.java
+++ Map/src/main/java/mapsystem/MapProcessor.java
@@ -2,19 +2,35 @@ package mapsystem;
 
 import common.data.GameData;
 import common.data.World;
+import common.data.entities.weapon.WeaponSPI;
 import common.data.entities.zombie.ZombieSPI;
 import common.services.IEntityProcessingService;
 
-public class MapProcessor implements IEntityProcessingService {
-    MapPlugin mapPlugin = new MapPlugin();
+import java.util.Collection;
+import java.util.ServiceLoader;
 
+import static java.util.stream.Collectors.toList;
+
+public class MapProcessor implements IEntityProcessingService {
     @Override
     public void process(GameData gameData, World world) {
-        if ((gameData.getGameTime() % 15 <= gameData.getDelta()) && (gameData.getGameTime() > 1)) {
+        if ((gameData.getGameTime() % 5 <= gameData.getDelta()) && (gameData.getGameTime() > 1)) {
             // Spawn a zombie
-            for (ZombieSPI zombie : mapPlugin.getZombieSPI()) {
-                world.addEntity(zombie.createZombie(12 * 32, 12 * 32));
+            for (ZombieSPI zombie : getZombieSPI()) {
+                world.addEntity(zombie.createZombie(13 * 32, 13 * 32));
+            }
+            //Spawn a weapon
+            for (WeaponSPI weapon : getWeaponSPI()) {
+                world.addEntity(weapon.createWeapon(gameData.getDisplayWidth()/2+64, gameData.getDisplayHeight()/2+64));
             }
         }
+    }
+
+    protected Collection<? extends WeaponSPI> getWeaponSPI() {
+        return ServiceLoader.load(WeaponSPI.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+
+    protected Collection<? extends ZombieSPI> getZombieSPI() {
+        return ServiceLoader.load(ZombieSPI.class).stream().map(ServiceLoader.Provider::get).collect(toList());
     }
 }
