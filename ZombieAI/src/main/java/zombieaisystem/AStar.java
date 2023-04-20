d1fc4ac75cc68c6d8b3cb4dac43642661f282a49
LaugeSolvang <lasol21@student.sdu.dk>
2023-04-21 00:35:15 +0200

    Actually works now as well as the other

--- ZombieAI/src/main/java/zombieaisystem/AStar.java
+++ ZombieAI/src/main/java/zombieaisystem/AStar.java
@@ -1,6 +1,7 @@
 package zombieaisystem;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AStar {
     public static void main(String[] args) {
@@ -16,18 +17,34 @@ public class AStar {
                 }
             }
         }
-        AStar aStar = new AStar();
-        System.out.println(aStar.treeSearch(map, new int[]{2,2},new int[]{10,10}));
-        //STATE_SPACE = generateStateSpace(map);
-        //printStateSpace(STATE_SPACE);
+        AStar astar = new AStar();
+
+        long totalDuration = 0;
+
+        for (int i = 0; i < 30; i++) {
+            long startTime = System.nanoTime();
+
+            astar.treeSearch(map, "1,1", "10,10");
+
+            long endTime = System.nanoTime();
+            long duration = (endTime - startTime);  // in nanoseconds
+            totalDuration += duration;
+        }
+
+        long averageDuration = totalDuration / 30;
+        System.out.println("Average execution time: " + averageDuration + " ns");
+
+        System.out.println(astar.treeSearch(map, "1,1", "10,10"));
+
     }
+
     class Node {
-        public int[] STATE;
+        public String STATE;
         public Node PARENT_NODE;
         public int DEPTH;
         public int COST;
 
-        public Node(int[] state, Node parent, int depth, int cost) {
+        public Node(String state, Node parent, int depth, int cost) {
             STATE = state;
             PARENT_NODE = parent;
             DEPTH = depth;
@@ -44,123 +61,138 @@ public class AStar {
             }
             return path;
         }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "STATE='" + STATE + '\'' +
+                    ", DEPTH=" + DEPTH +
+                    ", COST=" + COST +
+                    '}';
+        }
+    }
+    private static Map<String, List<String>> STATE_SPACE;
+    private Map<String, Double> HEURISTICS;
+
+    public ArrayList<Node> treeSearch(int[][] grid, String INITIAL_STATE, String GOAL_STATE) {
+        if (STATE_SPACE == null) {
+            STATE_SPACE = generateStateSpace(grid);
+        }
+        if (HEURISTICS == null) {
+            HEURISTICS = generateHeuristics(grid, GOAL_STATE);
+        }
+
+        ArrayList<Node> fringe = new ArrayList<>();
+        Node initialNode = new Node(INITIAL_STATE, null, 0, 0);
+        Set<String> visited = new HashSet<>();
+        visited.add(initialNode.STATE);
+        fringe = INSERT(initialNode, fringe);
+        while (!fringe.isEmpty()) {
+            Node node = REMOVE_FIRST(fringe);
+            if (node.STATE.equals(GOAL_STATE)) {
+                return node.path();
+            }
+            ArrayList<Node> children = EXPAND(node);
+            for (Node child : children) {
+                if (!visited.contains(child.STATE)) {
+                    visited.add(child.STATE);
+                    child.COST = node.COST + 1;
+                    fringe = INSERT(child, fringe);
+                }
+
+            }
+            fringe.sort(Comparator.comparingDouble(x -> HEURISTICS.get(x.STATE)));
+
+            //System.out.println("fringe: " + fringe);
+        }
+        return null;
     }
 
-
-        private static Map<int[], List<int[]>> STATE_SPACE;
-        private Map<int[], Integer> HEURISTICS;
-
-        public ArrayList<Node> treeSearch(int[][] grid, int[] INITIAL_STATE, int[] GOAL_STATE) {
-            if (STATE_SPACE == null) {
-                STATE_SPACE = generateStateSpace(grid);
-            }
-            if (HEURISTICS == null) {
-                HEURISTICS = generateHeuristics(grid, GOAL_STATE);
-            }
-
-            ArrayList<Node> fringe = new ArrayList<>();
-            Node initialNode = new Node(INITIAL_STATE, null, 0, 0);
-            fringe = INSERT(initialNode, fringe);
-            while (!fringe.isEmpty()) {
-                Node node = REMOVE_FIRST(fringe);
-                if (java.util.Arrays.equals(node.STATE, GOAL_STATE)) {
-                    return node.path();
-                }
-                ArrayList<Node> children = EXPAND(node);
-                fringe = INSERT_ALL(children, fringe);
-                for (Node child : children) {
-                    child.COST = node.COST + 1;
-                }
-                fringe.sort(Comparator.comparingInt(x -> HEURISTICS.get(x.STATE) + x.COST));
-                System.out.println("fringe: " + fringe);
-            }
-            return null;
+    public ArrayList<Node> EXPAND(Node node) {
+        ArrayList<Node> successors = new ArrayList<>();
+        List<String> children = successor_fn(node.STATE);
+        for (String child : children) {
+            Node s = new Node(child, node, node.DEPTH + 1, 0);
+            successors = INSERT(s, successors);
         }
+        return successors;
+    }
 
-        public ArrayList<Node> EXPAND(Node node) {
-            ArrayList<Node> successors = new ArrayList<>();
-            List<int[]> children = successor_fn(node.STATE);
-            for (int[] child : children) {
-                Node s = new Node(child, node, node.DEPTH + 1, 0);
-                successors = INSERT(s, successors);
-            }
-            return successors;
-        }
+    public static ArrayList<Node> INSERT(Node node, ArrayList<Node> queue) {
+        queue.add(node);
+        return queue;
+    }
 
-        public static ArrayList<Node> INSERT(Node node, ArrayList<Node> queue) {
-            queue.add(node);
-            return queue;
-        }
+    public static ArrayList<Node> INSERT_ALL(ArrayList<Node> list, ArrayList<Node> queue) {
+        queue.addAll(list);
+        return queue;
+    }
 
-        public static ArrayList<Node> INSERT_ALL(ArrayList<Node> list, ArrayList<Node> queue) {
-            queue.addAll(list);
-            return queue;
-        }
+    public static Node REMOVE_FIRST(ArrayList<Node> queue) {
+        return queue.remove(0);
+    }
 
-        public static Node REMOVE_FIRST(ArrayList<Node> queue) {
-            return queue.remove(0);
-        }
+    public static List<String> successor_fn(String state) {
+        return STATE_SPACE.get(state);
+    }
 
-        public static List<int[]> successor_fn(int[] state) {
-            System.out.println(java.util.Arrays.toString(state));
-            System.out.println(STATE_SPACE.get(state));
-            return STATE_SPACE.get(state);
-        }
-
-        public static Map<int[], List<int[]>> generateStateSpace(int[][] grid) {
-            Map<int[], List<int[]>> stateSpace = new HashMap<>();
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] != 1) {
-                        int[] state = new int[]{i, j};
-                        stateSpace.put(state, generateNeighbours(grid, state));
-                    }
+    public static Map<String, List<String>> generateStateSpace(int[][] grid) {
+        Map<String, List<String>> stateSpace = new HashMap<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != 1) {
+                    String state = i + "," + j;
+                    stateSpace.put(state, generateNeighbours(grid, state));
                 }
             }
-            return stateSpace;
         }
+        return stateSpace;
+    }
 
-        // Method to get a list of a node's neighbors on the grid
-        private static List<int[]> generateNeighbours(int[][] grid, int[] STATE) {
-            List<int[]> neighbors = new ArrayList<>();
-            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-            for (int[] dir : directions) {
-                int x = STATE[0] + dir[0];
-                int y = STATE[1] + dir[1];
-                if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
-                    continue;
-                }
-                if (grid[x][y] == 1) {
-                    continue;
-                }
-                neighbors.add(new int[]{x,y});
+    // Method to get a list of a node's neighbors on the grid
+    private static List<String> generateNeighbours(int[][] grid, String STATE) {
+        List<String> neighbors = new ArrayList<>();
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        String[] parts = STATE.split(",");
+        int x = Integer.parseInt(parts[0]);
+        int y = Integer.parseInt(parts[1]);
+        for (int[] dir : directions) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            if (newX < 0 || newX >= grid.length || newY < 0 || newY >= grid[0].length) {
+                continue;
             }
-            return neighbors;
-        }
-
-        public Map<int[], Integer> generateHeuristics(int[][] grid, int[] GOAL_STATE) {
-            Map<int[], Integer> heuristics = new HashMap<>();
-            for (int x = 0; x < grid.length; x++) {
-                for (int y = 0; y < grid[x].length; y++) {
-                    int dx = Math.abs(x - GOAL_STATE[0]);
-                    int dy = Math.abs(y - GOAL_STATE[1]);
-                    heuristics.put(new int[]{x, y}, dx + dy);
-                }
+            if (grid[newX][newY] == 1) {
+                continue;
             }
-            return heuristics;
+            neighbors.add(newX + "," + newY);
         }
+        return neighbors;
+    }
 
-    public static void printStateSpace(Map<int[], List<int[]>> stateSpace) {
-        TreeSet<int[]> sortedStates = new TreeSet<>(Comparator.comparingInt((int[] o) -> o[0]).thenComparingInt(o -> o[1]));
+    public static Map<String, Double> generateHeuristics(int[][] grid, String GOAL_STATE) {
+        Map<String, Double> heuristics = new HashMap<>();
+        String[] goalParts = GOAL_STATE.split(",");
+        int dx = Integer.parseInt(goalParts[0]);
+        int dy = Integer.parseInt(goalParts[1]);
+        for (int x = 0; x < grid.length; x++) {
+            for (int y = 0; y < grid[x].length; y++) {
+                heuristics.put(x + "," + y, Math.sqrt(Math.pow(dx - x, 2) + Math.pow(dy - y, 2)));
+            }
+        }
+        return heuristics;
+    }
+
+    public static void printStateSpace(Map<String, List<String>> stateSpace) {
+        TreeSet<String> sortedStates = new TreeSet<>(Comparator.comparing((String o) -> o));
         sortedStates.addAll(stateSpace.keySet());
 
-        for (int[] state : sortedStates) {
-            System.out.print("State: " + Arrays.toString(state) + " Neighbours: ");
-            List<int[]> neighbours = stateSpace.get(state);
-            for (int[] neighbour : neighbours) {
-                System.out.print(Arrays.toString(neighbour) + " ");
+        for (String state : sortedStates) {
+            System.out.print("State: [" + state + "] Neighbours: ");
+            List<String> neighbours = stateSpace.get(state);
+            for (String neighbour : neighbours) {
+                System.out.print("["+neighbour + "] ");
             }
             System.out.println();
         }
-    }
-}
+    }}
