8b6bb0deb87184532395f5d8900ab715242ca02a
LaugeSolvang <lasol21@student.sdu.dk>
2023-04-25 21:11:35 +0200

    There can now be spawned weapons and zombies randomly on the map

--- Map/src/main/java/mapsystem/MapProcessor.java
+++ Map/src/main/java/mapsystem/MapProcessor.java
@@ -7,6 +7,8 @@ import common.data.entities.zombie.ZombieSPI;
 import common.services.IEntityProcessingService;
 
 import java.util.Collection;
+import java.util.Objects;
+import java.util.Random;
 import java.util.ServiceLoader;
 
 import static java.util.stream.Collectors.toList;
@@ -14,16 +16,64 @@ import static java.util.stream.Collectors.toList;
 public class MapProcessor implements IEntityProcessingService {
     @Override
     public void process(GameData gameData, World world) {
-        if ((gameData.getGameTime() % 5 <= gameData.getDelta()) && (gameData.getGameTime() > 1)) {
-            // Spawn a zombie
-            for (ZombieSPI zombie : getZombieSPI()) {
-                world.addEntity(zombie.createZombie(13 * 32, 13 * 32));
-            }
-            //Spawn a weapon
-            for (WeaponSPI weapon : getWeaponSPI()) {
-                world.addEntity(weapon.createWeapon(gameData.getDisplayWidth()/2+64, gameData.getDisplayHeight()/2+64));
+        int numZombies = 3;
+        int numWeapons = 1;
+        int zombieSpawnInterval = 5;
+        int weaponSpawnInterval = 10;
+        if ((gameData.getGameTime() % zombieSpawnInterval <= gameData.getDelta()) && (gameData.getGameTime() > 1)) {
+            spawnEntities(numZombies, "zombie", world, gameData);
+        }
+        if ((gameData.getGameTime() % weaponSpawnInterval <= gameData.getDelta()) && (gameData.getGameTime() > 1)) {
+            spawnEntities(numWeapons, "weapon", world, gameData);
+        }
+    }
+
+    private void spawnEntities(int numEntities, String entity, World world, GameData gameData) {
+        String[][] map = world.getMap();
+        //change so that tile size is stored in gameData
+        int tileSize = 32;
+        Random rand = new Random();
+
+        boolean isValidLocation = false;
+        int x = 0;
+        int y = 0;
+        while (!isValidLocation) {
+            x = rand.nextInt(map.length - 1);
+            y = rand.nextInt(map[0].length - 1);
+            isValidLocation = isValidLocation(map, x, y);
+        }
+
+        for (int i = 0; i < numEntities; i++) {
+            int xOffset = rand.nextInt(2 * tileSize) - tileSize;
+            int yOffset = rand.nextInt(2 * tileSize) - tileSize;
+            int entityX = (x * tileSize) + xOffset;
+            int entityY = (y * tileSize) + yOffset;
+
+            if (Objects.equals(entity, "weapon")) {
+                for (WeaponSPI weapon : getWeaponSPI()) {
+                    world.addEntity(weapon.createWeapon(entityX, entityY));
+                }
+            } else if (Objects.equals(entity, "zombie")) {
+                for (ZombieSPI zombie : getZombieSPI()) {
+                    world.addEntity(zombie.createZombie(entityX, entityY));
+                }
             }
         }
+    }
+    private boolean isValidLocation(String[][] map, int x, int y) {
+        int xStart = Math.max(x - 1, 0);
+        int xEnd = Math.min(x + 1, map.length - 1);
+        int yStart = Math.max(y - 1, 0);
+        int yEnd = Math.min(y + 2, map[0].length - 1);
+
+        for (int i = xStart; i <= xEnd; i++) {
+            for (int j = yStart; j <= yEnd; j++) {
+                if (Objects.equals(map[i][j], "obstruction")) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     protected Collection<? extends WeaponSPI> getWeaponSPI() {
